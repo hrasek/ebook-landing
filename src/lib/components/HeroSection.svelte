d5b95093221f96eb7418e24bd7a4e41ec2bfd189
Hrasek <hosekmotor@seznam.cz>
2025-01-20 14:44:30 +0100

    feat: Landing page styling.

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -16,7 +16,7 @@
     </div>
     <div class="hero-text white text-center">
         <h1>THIS EBOOK WILL</h1>
-        <h1>SAVE OU MONEY,</h1>
+        <h1>SAVE YOU MONEY,</h1>
         <h1 class="mb-m">TIME & NERVES</h1>
         <p class="light-grey">
             I moved to Spain seven years ago. The process was painful and difficult. I
@@ -26,3 +26,42 @@
         </p>
     </div>
 </section>
+<style>
+    .hero {
+        display: flex;
+        height: 100vh;
+        overflow-y: hidden;
+    }
+    .hero-showcase{
+        position: relative;
+        width: 50%;
+        padding: 100px 16px 0 16px;
+        background: linear-gradient(
+            135deg,
+            #e2cbff,
+            #e2cbff 25%,
+            #deeaff 50%,
+            #ece5ff 75%,
+            #927bce
+        );
+    }
+    .hero-text{
+        padding: 100px 5%;
+        width: 50%;
+        background-color: black;
+    }
+    .book-cover{
+        position: absolute;
+        right: 2%;
+        bottom: 0;
+        width: 65%;
+        transform: translateY(30%);
+    }
+    .phone-cover{
+        position: absolute;
+        width: 35%;
+        bottom: 0;
+        right: 52%;
+        transform: translateY(25%);
+    }
+</style>
